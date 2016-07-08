99a49bbbdf09ff6fd1c2bcde7737070323075174
TonyCurtisLives <testfirstdevelopment@gmail.com>
2016-07-07 18:19:41 -0600

    Done with throw and feed tests in master controller

--- test/spec/masterControllerSpec.ts
+++ test/spec/masterControllerSpec.ts
@@ -68,14 +68,24 @@ describe('In the file masterController.ts', () => {
         expect(sut.masterObjects[5].name).toEqual('table scraps');
       });
     });
-    describe('throwSomething method', () => {
-      it('should broadcast the throw event name', () => {
-        pending('finish in mocking module');
+    describe('broadcast related property', () => {
+      let broadcastObject = new vdog.DogObject('meh', false, false);
+      beforeEach(() => {
+        spyOn($rootScope, '$broadcast');
       });
-    });
-    describe('feedTheDog method', () => {
-      it('should broadcast the feed event name', () => {
-        pending('finish in mocking module');
+      describe('throwSomething', () => {
+        it('should broadcast the throw event name and the object thrown', () => {
+          sut.throwSomething(broadcastObject);
+          expect($rootScope.$broadcast).toHaveBeenCalledWith(
+            vdog.eventNames.masterThrow, broadcastObject);
+        });
+      });
+      describe('feedTheDog', () => {
+        it('should broadcast the feed event name and the object fed', () => {
+          sut.feedTheDog(broadcastObject);
+          expect($rootScope.$broadcast).toHaveBeenCalledWith(
+            vdog.eventNames.masterFeed, broadcastObject);
+        });
       });
     });
   });
